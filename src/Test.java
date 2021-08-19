e34d8f07041515fb3e95ed536d829187dacf7413
AndrewReutWF <52035537+AndrewReutWF@users.noreply.github.com>
2021-08-19 15:28:23 +0200

    First
    It is the first commit

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
+        System.out.println("Введите слово");
+        String toWrite = a.nextLine();
+        System.out.println("введенное слово "+ toWrite);
+    }
+
+}
