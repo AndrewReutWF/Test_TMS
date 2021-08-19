39461fa5740332f40707cea099109139fd34f7b7
AndrewReutWF <52035537+AndrewReutWF@users.noreply.github.com>
2021-08-19 15:29:25 +0200

    Second
    It is the second commit

--- src/Test.java
+++ src/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
     public static void main(String[] args) {
         Scanner a = new Scanner(System.in);
-        System.out.println("Введите слово");
+        System.out.println("Введите слово первое");
         String toWrite = a.nextLine();
         System.out.println("введенное слово "+ toWrite);
     }
